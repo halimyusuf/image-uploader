4cd864a7aaf1e41df37f2c37f428775f643cee06
halimyusuf <haleemyoosuph@gmail.com>
2021-09-23 21:44:18 +0100

    feat: can add and preview image

--- components/ImageUploader.js
+++ components/ImageUploader.js
@@ -3,28 +3,49 @@ import { useRef, useState } from "react";
 export default function ImageUploader() {
   const fileInputRef = useRef(null);
   const [filename, setFileName] = useState("");
+  const [picture, setPicture] = useState({});
 
-  function onChooseFile() {
+  function clickInputFile() {
     fileInputRef.current.click();
   }
 
+  const defaultImg = "/image.svg";
+
+  function onFileChange(e) {
+    const files = e.target.files;
+    setPicture({
+      picture: files.length ? files[0] : defaultImg,
+      picturePreview: files.length
+        ? URL.createObjectURL(e.target.files[0])
+        : defaultImg,
+    });
+  }
+
   function onInputFileClose(e) {}
+
   return (
     <div className="image-upload-cont">
       <div className="upload-text">Upload your image</div>
       <div className="upload-text-1">File should be Jpeg, Png,...</div>
       <div className="image-drop-cont">
-        <Image src="/image.svg" width="150" height="150" alt=".." />
+        <Image
+          src={`${
+            picture.picturePreview ? picture.picturePreview : defaultImg
+          }`}
+          width="200"
+          height="140"
+          alt=".."
+        />
         <div className="upload-text-2">Drag & Drop your image here</div>
       </div>
       <div className="upload-text-3">Or</div>
-      <div>
-        <div onClick={onChooseFile} className="choose-file-btn">
-          {" "}
-          Choose a file{" "}
+      <div className="img-upload-action">
+        <div onClick={clickInputFile} className="choose-file-btn">
+          <div className="choose-file-btn-text">Choose a file</div>
         </div>
         <div>
           <input
+            onChange={onFileChange}
             type="file"
             ref={fileInputRef}
             title="Choose a file"
